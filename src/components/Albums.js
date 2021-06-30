c482da3a740880aeaba3dc7714ada1fa04e979e6
ianlee92 <ethian9227@gmail.com>
2021-07-01 08:17:25 +0900

    albumpage map

--- src/components/Albums.js
+++ src/components/Albums.js
@@ -1,13 +1,19 @@
 import React from 'react';
 import { View, Text, Pressable, ScrollView, StyleSheet } from 'react-native';
 import theme from '../../assets/themes/index';
-
+import albumPage from '../../assets/data/albumPage';
 
 const Albums = ({ navigation }) => {
   return (
     <>
       <ScrollView>
-        <View style={StyleSheet.albumContainer}></View>
+        <View style={StyleSheet.albumContainer}>
+          {albumPage.map((item, index) => 
+            <View key={index}>
+              <Text>{item.title}</Text>
+            </View>
+          )}
+        </View>
       </ScrollView>
     </>
   );
